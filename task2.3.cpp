39c991c2f15b19ee9d1171a57c55367220dc3a32
Во Чунг Кьен/ТКИ-141 <kienvtk29@gmail.com>
2025-12-08 13:59:12 +0300

    Add files via upload

---
+++ task2.3.cpp
@@ -0,0 +1,25 @@
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+double x, y, z;
+double getsum(const double x, const double y, const double z);
+double getsum(const double x, const double y, const double z)
+{
+	return x + y + z;
+}
+int main() 
+
+{
+	printf("enter first number ");
+	scanf_s("%lf", &x);
+	printf("enter second number ");
+	scanf_s("%lf", &y);
+	printf("enter third number ");
+	scanf_s("%lf",&z);
+	if (getsum(x, y, z) > 0)
+		printf("The sum of x, y and z is positive %lf", getsum(x,y,z));
+	else if (getsum(x, y, z) < 0)
+		printf("The sum of x, y and z is negative %lf", getsum(x, y, z));
+	else
+		printf("The sum of x, y and z is zero");
+}
