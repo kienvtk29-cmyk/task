8a4eafa8f46c5409634b54b243f07082c681a5d3
Во Чунг Кьен/ТКИ-141 <kienvtk29@gmail.com>
2025-12-15 10:05:48 +0300

    Update task2.3.cpp

--- task2.3.cpp
+++ task2.3.cpp
@@ -1,26 +1,58 @@
 #include<math.h>
 #include<stdio.h>
 #include<stdlib.h>
-double x, y, z;
+/**
+ * @brief Считывает целое число с клавиатуры
+ * @return Введённое целое число
+ * @note Если ввод некорректен, программа завершает работу
+ */
+int getValue();
+
+/**
+ * @brief Вычисляет сумму трёх чисел x, y и z
+ * @param x Первое число
+ * @param y Второе число
+ * @param z Третье число
+ * @return Сумма x + y + z
+ */
 double getsum(const double x, const double y, const double z);
-double getsum(const double x, const double y, const double z)
-{
-	return x + y + z;
-}
-int main() 
+
+/**
+ * @brief Точка входа в программу
+ * @return 0, если программа выполнена корректно
+ */
+int main()
 
 {
+	
 	printf("enter first number ");
-	scanf_s("%lf", &x);
+	double x = getValue();
 	printf("enter second number ");
-	scanf_s("%lf", &y);
+	double y = getValue();
 	printf("enter third number ");
-	scanf_s("%lf",&z);
+	double z = getValue();
 	if (getsum(x, y, z) > 0)
 		printf("The sum of x, y and z is positive %lf", getsum(x,y,z));
 	else if (getsum(x, y, z) < 0)
 		printf("The sum of x, y and z is negative %lf", getsum(x, y, z));
 	else
 		printf("The sum of x, y and z is zero");
-	return 0;
 }
+int getValue()
+
+{
+	int value = 0;
+	if (!scanf_s("%d", &value)) {
+		printf("error\n");
+		exit(1);
+	}
+	return value;
+}
+double getsum(const double x, const double y, const double z)
+{
+	return x + y + z;
+}
+
+
+
+
