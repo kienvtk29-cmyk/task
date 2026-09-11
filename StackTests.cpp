e6e4acd99c3530d7a198e6a09a51792cdebedb0b
Во Чунг Кьен/ТКИ-141 <kienvtk29@gmail.com>
2026-09-11 18:36:26 +0300

    Update StackTests.cpp

--- StackTests.cpp
+++ StackTests.cpp
@@ -1,6 +1,6 @@
-﻿#include "CppUnitTest.h"
+#include "CppUnitTest.h"
 #include "../StackLibrary/Stack.h"
-
+#include <utility>
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace StackTests
@@ -9,43 +9,28 @@ namespace StackTests
     {
     public:
 
-        TEST_METHOD(PushTest)
-        {
-            Stack stack;
-
-            stack.push(10);
-            stack.push(20);
-
-            Assert::AreEqual((size_t)2, stack.getSize());
-        }
-
-        TEST_METHOD(PeekTest)
-        {
-            Stack stack{ 1,2,3 };
-
-            Assert::AreEqual(3, stack.peek());
-        }
-
-        TEST_METHOD(PopTest)
-        {
-            Stack stack{ 10,20,30 };
-
-            int value = stack.pop();
-
-            Assert::AreEqual(30, value);
-            Assert::AreEqual((size_t)2, stack.getSize());
-        }
-
-        TEST_METHOD(EmptyTest)
+        // Тест конструктора по умолчанию
+        TEST_METHOD(DefaultConstructorTest)
         {
             Stack stack;
 
             Assert::IsTrue(stack.empty());
+            Assert::AreEqual(0, stack.getSize());
         }
 
+        // Тест конструктора со списком инициализации
+        TEST_METHOD(InitializerListConstructorTest)
+        {
+            Stack stack{ 1, 2, 3 };
+
+            Assert::AreEqual(3, stack.getSize());
+            Assert::AreEqual(3, stack.peek());
+        }
+
+        // Тест конструктора копирования
         TEST_METHOD(CopyConstructorTest)
         {
-            Stack stack{ 1,2,3 };
+            Stack stack{ 1, 2, 3 };
 
             Stack copy(stack);
 
@@ -53,6 +38,60 @@ namespace StackTests
                 stack.toString(),
                 copy.toString()
             );
+
+            Assert::AreEqual(
+                stack.getSize(),
+                copy.getSize()
+            );
+        }
+
+        // Тест конструктора перемещения
+        TEST_METHOD(MoveConstructorTest)
+        {
+            Stack stack{ 1, 2, 3 };
+
+            Stack moved(std::move(stack));
+
+            Assert::AreEqual(3, moved.getSize());
+            Assert::AreEqual(3, moved.peek());
+        }
+
+        // Тест метода push()
+        TEST_METHOD(PushTest)
+        {
+            Stack stack;
+
+            stack.push(10);
+            stack.push(20);
+
+            Assert::AreEqual(2, stack.getSize());
+        }
+
+        // Тест метода peek()
+        TEST_METHOD(PeekTest)
+        {
+            Stack stack{ 1, 2, 3 };
+
+            Assert::AreEqual(3, stack.peek());
+        }
+
+        // Тест метода pop()
+        TEST_METHOD(PopTest)
+        {
+            Stack stack{ 10, 20, 30 };
+
+            int value = stack.pop();
+
+            Assert::AreEqual(30, value);
+            Assert::AreEqual(2, stack.getSize());
+        }
+
+        // Тест метода empty()
+        TEST_METHOD(EmptyTest)
+        {
+            Stack stack;
+
+            Assert::IsTrue(stack.empty());
         }
     };
 }
